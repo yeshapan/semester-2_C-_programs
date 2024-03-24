3f09547f7f32d9c4322012dd8cb1e75c47673fbc
Yesha Pandya <yesha.pandya@outlook.com>
2024-03-24 15:05:30 +0530

    Create 07_area_volume_of_room.cpp

---
+++ 07_area_volume_of_room.cpp
@@ -0,0 +1,42 @@
+//create a class "ROOM" having data members l, b and h and calculate it's area and volume
+
+//functions defined inside class
+#include <iostream>
+using namespace std;
+
+class room{
+    private:
+        int length, breadth, height;
+    public:
+        //function to get dimensions of the room from the user
+        void getdata(){
+            cout<<"Enter length of room: ";
+            cin>>length;
+            cout<<"Enter breadth of room: ";
+            cin>>breadth;
+            cout<<"Enter height of room: ";
+            cin>>height;
+        }
+
+        //function to calculate area of the room
+        void calculate_area(){
+            int area;
+            area=length*breadth;
+            cout<<"Area of the room is "<<area<<endl;
+        }
+        
+        //function to calculate the volume of the room
+        void calculate_volume(){
+            int volume;
+            volume=length*breadth*height;
+            cout<<"Volume of the room is "<<volume<<endl;
+        }
+};
+
+int main(){
+    room object1;
+    object1.getdata();
+    object1.calculate_area();
+    object1.calculate_volume();
+    return 0;
+}
